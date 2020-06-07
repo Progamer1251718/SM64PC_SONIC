e8ef15ddbd6e6b5bd6934ae6d8f4f7cbc3372643
Mario <Jgmario9501@gmail.com>
2020-06-06 19:08:53 -0600

    Add files via upload

---
+++ FILE_TEST/mario/header.h
@@ -0,0 +1,15 @@
+extern Vtx mario_SONIC_mesh_vtx_0[95];
+extern Gfx mario_SONIC_mesh_tri_0[];
+extern Vtx mario_SONIC_mesh_vtx_1[46];
+extern Gfx mario_SONIC_mesh_tri_1[];
+extern Vtx mario_SONIC_mesh_vtx_2[865];
+extern Gfx mario_SONIC_mesh_tri_2[];
+extern Vtx mario_SONIC_mesh_vtx_3[146];
+extern Gfx mario_SONIC_mesh_tri_3[];
+extern Vtx mario_SONIC_mesh_vtx_4[41];
+extern Gfx mario_SONIC_mesh_tri_4[];
+extern Vtx mario_SONIC_mesh_vtx_5[61];
+extern Gfx mario_SONIC_mesh_tri_5[];
+
+extern Gfx mario_SONIC_mesh[];
+
