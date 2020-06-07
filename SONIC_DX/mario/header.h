d33fcd2b2ebbaefef4a1645e651f8ff74cf270f8
Mario <Jgmario9501@gmail.com>
2020-06-07 01:06:49 -0600

    Sonic DX
    Things need to be fixed.

---
+++ SONIC_DX/mario/header.h
@@ -0,0 +1,25 @@
+extern Vtx mario_object_5695944_002_mesh_vtx_0[54];
+extern Gfx mario_object_5695944_002_mesh_tri_0[];
+extern Vtx mario_object_5695944_002_mesh_vtx_1[72];
+extern Gfx mario_object_5695944_002_mesh_tri_1[];
+extern Vtx mario_object_5695944_002_mesh_vtx_2[230];
+extern Gfx mario_object_5695944_002_mesh_tri_2[];
+extern Vtx mario_object_5695944_002_mesh_vtx_3[70];
+extern Gfx mario_object_5695944_002_mesh_tri_3[];
+extern Vtx mario_object_5695944_002_mesh_vtx_4[93];
+extern Gfx mario_object_5695944_002_mesh_tri_4[];
+extern Vtx mario_object_5695944_002_mesh_vtx_5[367];
+extern Gfx mario_object_5695944_002_mesh_tri_5[];
+extern Vtx mario_object_5695944_002_mesh_vtx_6[379];
+extern Gfx mario_object_5695944_002_mesh_tri_6[];
+extern Vtx mario_object_5695944_002_mesh_vtx_7[129];
+extern Gfx mario_object_5695944_002_mesh_tri_7[];
+extern Vtx mario_object_5695944_002_mesh_vtx_8[983];
+extern Gfx mario_object_5695944_002_mesh_tri_8[];
+extern Vtx mario_object_5695944_002_mesh_vtx_9[325];
+extern Gfx mario_object_5695944_002_mesh_tri_9[];
+extern Vtx mario_object_5695944_002_mesh_vtx_10[8];
+extern Gfx mario_object_5695944_002_mesh_tri_10[];
+
+extern Gfx mario_object_5695944_002_mesh[];
+
