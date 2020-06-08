fdcd5374e1905f1f14a68d7c6a7c23ec2de35122
Mario <Jgmario9501@gmail.com>
2020-06-07 22:32:25 -0600

    Add files via upload

---
+++ Tails_DX/mario/header.h
@@ -0,0 +1,17 @@
+extern Vtx mario_object_41FE042_mesh_vtx_0[469];
+extern Gfx mario_object_41FE042_mesh_tri_0[];
+extern Vtx mario_object_41FE042_mesh_vtx_1[174];
+extern Gfx mario_object_41FE042_mesh_tri_1[];
+extern Vtx mario_object_41FE042_mesh_vtx_2[48];
+extern Gfx mario_object_41FE042_mesh_tri_2[];
+extern Vtx mario_object_41FE042_mesh_vtx_3[802];
+extern Gfx mario_object_41FE042_mesh_tri_3[];
+extern Vtx mario_object_41FE042_mesh_vtx_4[195];
+extern Gfx mario_object_41FE042_mesh_tri_4[];
+extern Vtx mario_object_41FE042_mesh_vtx_5[786];
+extern Gfx mario_object_41FE042_mesh_tri_5[];
+extern Vtx mario_object_41FE042_mesh_vtx_6[236];
+extern Gfx mario_object_41FE042_mesh_tri_6[];
+
+extern Gfx mario_object_41FE042_mesh[];
+
